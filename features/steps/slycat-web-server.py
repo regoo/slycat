9e7383f648e65d2cebc31c16de1028ce6bfb8034
David R Lochridge <drlochr@sandia.gov>
2015-07-30 15:17:16 -0600

    Needed to import exception

--- features/steps/slycat-web-server.py
+++ features/steps/slycat-web-server.py
@@ -16,6 +16,7 @@ import re
 from pyvirtualdisplay import Display
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
